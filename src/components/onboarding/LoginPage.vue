9e08bed1881701a45f593e623e3c7b5deedd2b25
EromhonseleAtinse <atinseeromhonsele@gmail.com>
2020-08-03 13:07:43 +0100

    refactored login page

--- src/components/onboarding/LoginPage.vue
+++ src/components/onboarding/LoginPage.vue
@@ -1,46 +1,51 @@
 <template>
-  <v-card class="login-card mx-auto">
-    <v-card-title>
-      <h3 class="login-title">Welcome Back!</h3>
-    </v-card-title>
-    <v-card-text>
-      <ValidationObserver v-slot="{ handleSubmit }">
-        <v-form class="login-form" @submit.prevent="handleSubmit(handleLogin)">
-          <div v-if="errorMsg">
-            <span class="err text-xl-center">{{errorMsg}}</span>
-          </div>
-          <Loader :loading="loading" :message="message" />
+  <v-row>
+    <v-col cols="12" md="6" class="pl-16">
+      <div class="login-coloumn-left mx-auto">
+        <ValidationObserver v-slot="{ handleSubmit }">
+          <v-form class="my-auto" @submit.prevent="handleSubmit(handleLogin)">
+            <h3 class="login-title">Welcome back</h3>
+            <div v-if="errorMsg">
+              <span class="err text-xl-center">{{errorMsg}}</span>
+            </div>
+            <Loader :loading="loading" :message="message" />
 
-          <ValidationProvider name="E-mail" rules="required|email" v-slot="{ errors }">
-            <span class="err">{{ errors[0] }}</span>
-            <v-text-field
-              label="Email Address"
-              name="emailAddress"
-              v-model="user.email"
-              outlined
-              color="red"
-              class="register-input"
-            ></v-text-field>
-          </ValidationProvider>
-          <ValidationProvider name="password" rules="required|min:6" v-slot="{ errors }">
-            <span class="err">{{ errors[0] }}</span>
-            <v-text-field
-              :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="() => (value = !value)"
-              :type="value ? 'password' : 'text'"
-              label="Password"
-              name="password"
-              v-model="user.password"
-              outlined
-              color="red"
-              class="register-input"
-            ></v-text-field>
-          </ValidationProvider>
-          <SubmitButton button-name="Login" class="login-btn" />
-        </v-form>
-      </ValidationObserver>
-    </v-card-text>
-  </v-card>
+            <ValidationProvider name="E-mail" rules="required|email" v-slot="{ errors }">
+              <span class="err">{{ errors[0] }}</span>
+              <v-text-field
+                label="Email Address"
+                name="emailAddress"
+                v-model="user.email"
+                outlined
+                color="red"
+                class="register-input"
+              ></v-text-field>
+            </ValidationProvider>
+            <ValidationProvider name="password" rules="required|min:6" v-slot="{ errors }">
+              <span class="err">{{ errors[0] }}</span>
+              <v-text-field
+                :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="() => (value = !value)"
+                :type="value ? 'password' : 'text'"
+                label="Password"
+                name="password"
+                v-model="user.password"
+                outlined
+                color="red"
+                class="register-input"
+              ></v-text-field>
+            </ValidationProvider>
+            <SubmitButton button-name="Login" class="login-btn" />
+          </v-form>
+        </ValidationObserver>
+      </div>
+    </v-col>
+    <v-col cols="12" md="6" class="d-none d-md-flex login-coloumn">
+      <div class="mx-auto">
+        <img src="../../assets/desk-illo.svg" width="600px" />
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -95,15 +100,18 @@ export default {
 </script>
 
 <style scoped>
-.login-card {
-  margin-top: 80px;
-  width: 500px;
-
-  /*border: 1px solid #F8F8F8;*/
-  /*box-sizing: border-box;*/
-  /*box-shadow: 0px 1px 2px rgba(43, 28, 28, 0.15);*/
-  border-radius: 10px;
+.login-coloumn {
+  background-color: #fff0ce;
+  min-height: 92vh;
 }
+.login-coloumn div img {
+  margin-top: 10rem !important;
+}
+.login-coloumn-left {
+  margin-top: 10rem !important;
+  width: 30rem !important;
+}
+
 .login-title {
   font-style: normal;
   font-weight: 500;
@@ -119,10 +127,7 @@ export default {
 
   color: #2b1c1c;
 }
-.login-form {
-  margin-right: 17%;
-  margin-left: 17%;
-}
+
 .v-text-field {
   height: 70px !important;
   border-radius: 6px !important;
@@ -135,18 +140,5 @@ export default {
   color: red;
   font-size: 13px;
   margin-bottom: 5px;
-}
-@media only screen and (max-width: 600px) {
-  .login-form {
-    margin-left: 7%;
-    margin-right: 7%;
-  }
-  .login-card {
-    border-radius: 0 !important;
-    box-shadow: none !important;
-  }
-  .login-title {
-    font-size: 20px;
-  }
 }
 </style>
