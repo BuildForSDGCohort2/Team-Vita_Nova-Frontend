7a44a5628390bec235f44280e80777dcada1a4d4
Tundeh <jamiu4freedom@gmail.com>
2020-10-01 15:33:28 +0100

    create store instance and wrapp components in redux provider

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createBrowserHistory} from "history";
-import {Router, Route, Switch} from "react-router-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
 
+import configureStore from "./redux/configureStore";
 import "./assets/scss/material-kit-react.scss";
 import LandingPage from "./views/LandingPage/LandingPage.js";
 import LoginPage from "./views/LoginPage/LoginPage.js";
 import ProfilePage from "./views/ProfilePage/ProfilePage.js";
 import Signup from "./views/SignupPage/SignupPage.js";
 
+const store = configureStore();
+
 var hist = createBrowserHistory();
 
 ReactDOM.render(
-    <Router history = {hist}>
-        <Switch>
-            <Route path="/landing-page" component={LandingPage}/>
-            <Route path="/login-page" component={LoginPage}/>
-            <Route path="/profile-page" component={ProfilePage}/>
-            <Route path="/signup-page" component={Signup}/>
-            <Route path="/" component={LandingPage} />
-        </Switch>
-    </Router>,
-    document.getElementById("root")
+  <ReduxProvider store={store}>
+    <Router history={hist}>
+      <Switch>
+        <Route path="/landing-page" component={LandingPage} />
+        <Route path="/login-page" component={LoginPage} />
+        <Route path="/profile-page" component={ProfilePage} />
+        <Route path="/signup-page" component={Signup} />
+        <Route path="/" component={LandingPage} />
+      </Switch>
+    </Router>
+  </ReduxProvider>,
+  document.getElementById("root")
 );
